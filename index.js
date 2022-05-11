5cbb7d608217974a260effd50ebdfcb87065e27b
Abner <abnerdasilvatavares@gmail.com>
2022-05-10 22:17:16 -0300

    Porta dinamica

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ app.use(express.json());
 app.use(cors());
 app.use(authRouter);
 
-app.listen(5000);
+const PORTA = process.env.PORT || 5000;
+
+app.listen(PORTA, () => {
+    console.log("Server running on port " + process.env.PORT);
+});
