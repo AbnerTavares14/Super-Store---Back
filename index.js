3271bbfe1e5a2b34f8fed446d22ad2368d966b7b
Abner <abnerdasilvatavares@gmail.com>
2022-05-11 16:53:07 -0300

    Post para adicionar produtos e get para recuperar todos os produtos de uma vez

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import authRouter from './src/Routers/authRouter.js';
+import productsRouter from './src/Routers/productsRouter.js';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(authRouter);
+app.use(productsRouter);
 
 const PORTA = process.env.PORT || 5000;
 
--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,5 +1,6 @@
 import { ObjectId } from "mongodb";
 import db from "../db.js";
+import joi from "joi";
 
 export async function singleProduct(req, res) {
     const { productId } = req.params;
@@ -36,5 +37,39 @@ export async function addToCart(req, res) {
         res.sendStatus(201);
     } catch (error) {
         console.log(error);
+    }
+}
+
+export async function addProducts(req, res) {
+    const { body } = req;
+    const productsSchema = joi.object({
+        name: joi.string().required(),
+        type: joi.string().required(),
+        description: joi.string().required(),
+        price: joi.string().required(),
+        image: joi.string().pattern(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/).required()
+    })
+    const validation = productsSchema.validate(body, { abortEarly: true });
+    if (validation.error) {
+        console.log(validation.error.details);
+        res.sendStatus(406);
+    }
+
+    try {
+        await db.collection("products").insertOne(body);
+        res.sendStatus(201)
+    } catch (error) {
+        console.log("Falha em adicionar produto", error);
+        res.sendStatus(500);
+    }
+}
+
+export async function getAllProducts(req, res) {
+    try {
+        const products = await db.collection("products").find({}).toArray();
+        res.send(products);
+    } catch (err) {
+        console.log("Falha em enviar os produtos", err);
+        res.sendStatus(500);
     }
 }
