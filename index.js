2fc43c62718bf6a569cd99bc789053ded614a217
joltzz <guijsilva02@gmail.com>
2022-05-12 12:16:41 -0300

    controller de caterogia pronto

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import authRouter from './src/Routers/authRouter.js';
 import productsRouter from './src/Routers/productsRouter.js';
 import cartRouter from './src/Routers/cartRouter.js';
+import catergoryRouter from './src/Routers/categoryRouter.js';
 
 const app = express();
 app.use(express.json());
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(authRouter);
 app.use(productsRouter);
 app.use(cartRouter);
+app.use(catergoryRouter);
 
 const PORTA = process.env.PORT || 5000;
 
