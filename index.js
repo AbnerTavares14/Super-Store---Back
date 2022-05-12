5ce14d5baad30c4b874cf5ac00ddeaf41149acf7
Abner <abnerdasilvatavares@gmail.com>
2022-05-12 11:38:56 -0300

    Requisições do carrinho e middlewares que verifica o token

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import authRouter from './src/Routers/authRouter.js';
 import productsRouter from './src/Routers/productsRouter.js';
+import cartRouter from './src/Routers/cartRouter.js';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(authRouter);
 app.use(productsRouter);
+app.use(cartRouter);
 
 const PORTA = process.env.PORT || 5000;
 
---
+++ src/controllers/cartController.js
@@ -0,0 +1,35 @@
+import db from "../db.js";
+
+export async function addToCart(req, res) {
+    const product = req.body;
+    const user = res.locals.user;
+
+    try {
+        const hasCart = await db.collection("carts").findOne({ userId: user._id });
+        if (hasCart) {
+            await db
+                .collection("carts")
+                .updateOne({ userId: user._id }, { $push: { cart: product } });
+            return res.sendStatus(201);
+        }
+        await db.collection("carts").insertOne({
+            userId: user._id,
+            cart: [product],
+        });
+        res.sendStatus(201);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function getProductsOfCart(req, res) {
+    const user = res.locals.user;
+    try {
+        const products = await db.collection("carts").find({ userId: user._id }).toArray();
+        res.send(products);
+    } catch (err) {
+        console.log("Deu erro na obtenção dos produtos no carrinho", err);
+        res.sendStatus(500);
+    }
+}
+
